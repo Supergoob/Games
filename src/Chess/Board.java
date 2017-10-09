ec86c63245ecea1d59cce12de908825fe7fbf82b
Gabe <Gabe@Gabescomputer>
2017-10-08 21:17:13 -0600

    Board

---
+++ src/Chess/Board.java
@@ -0,0 +1,38 @@
+package Chess;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board {
+	public static void main(String[] args) {
+		JFrame theBoard = new JFrame();
+		theBoard.setTitle("chess Board");
+		theBoard.setSize(1200,700);
+		theBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container pane = theBoard.getContentPane();
+		pane.setLayout(new GridLayout(8,8));
+		for(int i=0;i<64;i++){
+			JPanel panel=new JPanel();
+			if(i<8||i>=16&&i<24||i>=32&&i<40||i>=48&&i<56){
+				if(i%2!=0){
+			panel.setBackground(Color.BLACK);
+				}else{
+					panel.setBackground(Color.WHITE);
+				}
+			}else{
+				if(i%2!=0){
+				panel.setBackground(Color.WHITE);
+				}else{
+					panel.setBackground(Color.BLACK);
+				}
+			}
+			pane.add(panel);
+		}
+		theBoard.setVisible(true);
+		
+	}
+}
